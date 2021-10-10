cdbdfec4d3fc4fee2ed66428b2af6c25dc3a63b7
Jurek Rostalsky <jurek.rostalsky@ovgu.de>
2021-10-10 21:50:53 +0200

    change outputformat to png

--- src/printimage.cpp
+++ src/printimage.cpp
@@ -5,95 +5,8 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
-#include <vector>
 
-// similar to
-// https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
-
-const size_t BYTES_PER_PIXEL  = 3;  /// red, green, & blue
-const size_t FILE_HEADER_SIZE = 14;
-const size_t INFO_HEADER_SIZE = 40;
-
-void generateBitmapImage(
-  unsigned char* image, size_t height, size_t width, const char* imageFileName);
-unsigned char* createBitmapFileHeader(int height, int stride);
-unsigned char* createBitmapInfoHeader(int height, int width);
-
-void generateBitmapImage(
-  unsigned char* image, size_t height, size_t width, const char* imageFileName) {
-  size_t widthInBytes = width * BYTES_PER_PIXEL;
-
-  unsigned char padding[3] = {0, 0, 0};
-  int paddingSize          = (4 - (widthInBytes) % 4) % 4;
-
-  size_t stride = (widthInBytes) + paddingSize;
-
-  FILE* imageFile = fopen(imageFileName, "wb");
-
-  unsigned char* fileHeader = createBitmapFileHeader(height, stride);
-  fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
-
-  unsigned char* infoHeader = createBitmapInfoHeader(height, width);
-  fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
-
-  for (size_t i = 0; i < height; i++) {
-    fwrite(image + (i * widthInBytes), BYTES_PER_PIXEL, width, imageFile);
-    fwrite(padding, 1, paddingSize, imageFile);
-  }
-
-  fclose(imageFile);
-}
-
-unsigned char* createBitmapFileHeader(int height, int stride) {
-  int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
-
-  static unsigned char fileHeader[] = {
-    0, 0,        /// signature
-    0, 0, 0, 0,  /// image file size in bytes
-    0, 0, 0, 0,  /// reserved
-    0, 0, 0, 0,  /// start of pixel array
-  };
-
-  fileHeader[0]  = (unsigned char) ('B');
-  fileHeader[1]  = (unsigned char) ('M');
-  fileHeader[2]  = (unsigned char) (fileSize);
-  fileHeader[3]  = (unsigned char) (fileSize >> 8);
-  fileHeader[4]  = (unsigned char) (fileSize >> 16);
-  fileHeader[5]  = (unsigned char) (fileSize >> 24);
-  fileHeader[10] = (unsigned char) (FILE_HEADER_SIZE + INFO_HEADER_SIZE);
-
-  return fileHeader;
-}
-
-unsigned char* createBitmapInfoHeader(int height, int width) {
-  static unsigned char infoHeader[] = {
-    0, 0, 0, 0,  /// header size
-    0, 0, 0, 0,  /// image width
-    0, 0, 0, 0,  /// image height
-    0, 0,        /// number of color planes
-    0, 0,        /// bits per pixel
-    0, 0, 0, 0,  /// compression
-    0, 0, 0, 0,  /// image size
-    0, 0, 0, 0,  /// horizontal resolution
-    0, 0, 0, 0,  /// vertical resolution
-    0, 0, 0, 0,  /// colors in color table
-    0, 0, 0, 0,  /// important color count
-  };
-
-  infoHeader[0]  = (unsigned char) (INFO_HEADER_SIZE);
-  infoHeader[4]  = (unsigned char) (width);
-  infoHeader[5]  = (unsigned char) (width >> 8);
-  infoHeader[6]  = (unsigned char) (width >> 16);
-  infoHeader[7]  = (unsigned char) (width >> 24);
-  infoHeader[8]  = (unsigned char) (height);
-  infoHeader[9]  = (unsigned char) (height >> 8);
-  infoHeader[10] = (unsigned char) (height >> 16);
-  infoHeader[11] = (unsigned char) (height >> 24);
-  infoHeader[12] = (unsigned char) (1);
-  infoHeader[14] = (unsigned char) (BYTES_PER_PIXEL * 8);
-
-  return infoHeader;
-}
+#include <pngwriter.h>
 
 // read the input data
 __int16_t* readimage(size_t& half_height, size_t& width, const char* filename) {
@@ -116,7 +29,7 @@ void printimage(
   int16_t* pixels = readimage(half_height, width, inputFilename);
 
   const size_t half_size = half_height * width;
-  const size_t size      = 2 * half_size;
+  const size_t height    = 2 * half_height;
   int maxiter            = 0;
   for (size_t i = 0; i < half_size; ++i) {
     if (pixels[i] > maxiter) {
@@ -124,26 +37,22 @@ void printimage(
     }
   }
 
-  unsigned char* image = (unsigned char*) malloc(size * BYTES_PER_PIXEL);
+  pngwriter png(width, height, 0, outputFilename);
 
   for (size_t i = 0; i < half_size; ++i) {
-    double intensity               = double(pixels[i]) / maxiter * 255;
-    unsigned char pixel_red        = std::round(red * intensity);    // red
-    unsigned char pixel_blue       = std::round(blue * intensity);   // blue
-    unsigned char pixel_green      = std::round(green * intensity);  // green
-    image[i * BYTES_PER_PIXEL + 2] = pixel_red;
-    image[i * BYTES_PER_PIXEL + 1] = pixel_green;
-    image[i * BYTES_PER_PIXEL + 0] = pixel_blue;
-    // abuse invariance under 180 degree rotaion
-    image[(size - 1 - i) * BYTES_PER_PIXEL + 2] = pixel_red;
-    image[(size - 1 - i) * BYTES_PER_PIXEL + 1] = pixel_green;
-    image[(size - 1 - i) * BYTES_PER_PIXEL + 0] = pixel_blue;
+    double intensity = double(pixels[i]) / maxiter;
+
+    const double pixel_red   = std::round(red * intensity);    // red
+    const double pixel_blue  = std::round(blue * intensity);   // blue
+    const double pixel_green = std::round(green * intensity);  // green
+
+    unsigned int x = i % width;
+    unsigned int y = i / width;
+    png.plot(x, y, pixel_red, pixel_green, pixel_blue);
+    png.plot(width - 1 - x, height - 1 - y, pixel_red, pixel_green, pixel_blue);
   }
+  png.close();
 
   free(pixels);
-
-  generateBitmapImage((unsigned char*) image, 2 * half_height, width, outputFilename);
-
-  free(image);
   std::cout << "maximum Iterations: " << maxiter << std::endl;
 }
